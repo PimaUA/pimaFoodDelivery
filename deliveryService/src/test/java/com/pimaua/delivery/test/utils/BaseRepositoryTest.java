a32145cbe1753622cabb61567c37433f3a86fbda
Pima <pimaua87@gmail.com>
2025-09-15 15:59:52 +0300

    refactored BaseRepositoryTest & application-test.yml files for all services.

--- deliveryService/src/test/java/com/pimaua/delivery/test/utils/BaseRepositoryTest.java
+++ deliveryService/src/test/java/com/pimaua/delivery/test/utils/BaseRepositoryTest.java
@@ -3,28 +3,31 @@ package com.pimaua.delivery.test.utils;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.time.Duration;
+
 @Testcontainers
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")
 public class BaseRepositoryTest {
     @Container
     static final MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
-            .withDatabaseName("testdb")
+            .withDatabaseName("deliveryservicedb")
             .withUsername("test")
-            .withPassword("test");
+            .withPassword("test")
+            .withStartupTimeout(Duration.ofMinutes(5));
 
     @DynamicPropertySource
     static void overrideProps(DynamicPropertyRegistry registry) {
-        if (!mysql.isRunning()) {
-            mysql.start();
-        }
+        mysql.start();
         registry.add("spring.datasource.url", mysql::getJdbcUrl);
         registry.add("spring.datasource.username", mysql::getUsername);
         registry.add("spring.datasource.password", mysql::getPassword);
