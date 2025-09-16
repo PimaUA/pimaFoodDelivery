8e148d4b937b844884e4b294c9c2ab2c991b37a4
Pima <pimaua87@gmail.com>
2025-09-17 00:02:48 +0300

    updated BaseRepositoryTest

--- paymentservice/src/test/java/com/pimaua/payment/test/utils/BaseRepositoryTest.java
+++ paymentservice/src/test/java/com/pimaua/payment/test/utils/BaseRepositoryTest.java
@@ -5,10 +5,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -27,17 +24,5 @@ public abstract class BaseRepositoryTest {
             .withDatabaseName("testdb")
             .withUsername("test")
             .withPassword("test")
-            .withStartupTimeout(Duration.ofMinutes(2))
-            .waitingFor(Wait.forListeningPort());
-
-    @DynamicPropertySource
-    static void overrideProps(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mysql::getJdbcUrl);
-        registry.add("spring.datasource.username", mysql::getUsername);
-        registry.add("spring.datasource.password", mysql::getPassword);
-        registry.add("spring.datasource.driver-class-name", mysql::getDriverClassName);
-        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
-        registry.add("spring.flyway.enabled", () -> "false");
-        registry.add("spring.datasource.type", () -> "com.zaxxer.hikari.HikariDataSource");
-    }
+            .withStartupTimeout(Duration.ofMinutes(2));
 }
