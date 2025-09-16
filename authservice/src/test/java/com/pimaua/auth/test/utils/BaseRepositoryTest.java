c3becdff2d4fe9bd1be626ec6b05ac2222eacc03
Pima <pimaua87@gmail.com>
2025-09-16 13:25:39 +0300

    updated BaseRepositoryTest

--- authservice/src/test/java/com/pimaua/auth/test/utils/BaseRepositoryTest.java
+++ authservice/src/test/java/com/pimaua/auth/test/utils/BaseRepositoryTest.java
@@ -1,13 +1,16 @@
 package com.pimaua.auth.test.utils;
 
 import org.junit.jupiter.api.TestInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -18,18 +21,25 @@ import java.time.Duration;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles("test")
+@ImportAutoConfiguration(exclude = FlywayAutoConfiguration.class)
 public abstract class BaseRepositoryTest {
+    private static final Logger log = LoggerFactory.getLogger(BaseRepositoryTest.class);
+
     @Container
     static final MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
             .withDatabaseName("authservicedb")
             .withUsername("test")
             .withPassword("test")
-            .withStartupTimeout(Duration.ofMinutes(5)) // wait up to 5 mins
-            .waitingFor(Wait.forListeningPort())      // ensures MySQL port is ready
-            .withReuse(true);                // reuse container between tests
+            .withStartupTimeout(Duration.ofMinutes(5)); // wait up to 5 mins
+
+    static {
+        log.info("Starting MySQL Testcontainer...");
+        mysql.start();
+    }
 
     @DynamicPropertySource
     static void overrideProps(DynamicPropertyRegistry registry) {
+        log.info("Overriding Spring Datasource properties for Testcontainers...");
         registry.add("spring.datasource.url", mysql::getJdbcUrl);
         registry.add("spring.datasource.username", mysql::getUsername);
         registry.add("spring.datasource.password", mysql::getPassword);
